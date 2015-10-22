caa6e66b4c8621ed194b1616116da23c8d639a9a
Garvan Keeley <garvankeeley@gmail.com>
2015-10-22 19:36:36 -0400

    JSContext runs on 'WebThread', make sure to call back to main thread

--- Client/LegacyJSContext.m
+++ Client/LegacyJSContext.m
@@ -3,7 +3,7 @@
 
 
 @interface FrameInfoWrapper : WKFrameInfo
-@property (nonatomic, retain) NSURLRequest* writableRequest;
+@property (atomic, retain) NSURLRequest* writableRequest;
 @end
 
 @implementation FrameInfoWrapper
@@ -21,9 +21,9 @@
 @end
 
 @interface LegacyScriptMessage: WKScriptMessage
-@property (nonatomic, retain) NSObject* writeableBody;
-@property (nonatomic, copy) NSString* writableName;
-@property (nonatomic, retain) NSURLRequest* request;
+@property (atomic, retain) NSObject* writeableBody;
+@property (atomic, copy) NSString* writableName;
+@property (atomic, retain) NSURLRequest* request;
 @end
 @implementation LegacyScriptMessage
 
@@ -51,30 +51,32 @@
                     handlerName:(NSString *)handlerName
                         handler:(id<WKScriptMessageHandler>)handler
 {
-  [webview stringByEvaluatingJavaScriptFromString:[NSString stringWithFormat:@""
-   "if (!window.hasOwnProperty('webkit')) {"
-     "Window.prototype.webkit = {};"
-     "Window.prototype.webkit.messageHandlers = {};"
-   "}"
-   "if (!window.webkit.messageHandlers.hasOwnProperty('%@'))"
-   "  Window.prototype.webkit.messageHandlers.%@ = {};", handlerName, handlerName]
-   ];
+  @synchronized(self) {
+    [webview stringByEvaluatingJavaScriptFromString:[NSString stringWithFormat:@""
+     "if (!window.hasOwnProperty('webkit')) {"
+       "Window.prototype.webkit = {};"
+       "Window.prototype.webkit.messageHandlers = {};"
+     "}"
+     "if (!window.webkit.messageHandlers.hasOwnProperty('%@'))"
+     "  Window.prototype.webkit.messageHandlers.%@ = {};", handlerName, handlerName]
+     ];
 
-  JSContext* context = [webview valueForKeyPath:@"documentView.webView.mainFrame.javaScriptContext"];
+    JSContext* context = [webview valueForKeyPath:@"documentView.webView.mainFrame.javaScriptContext"];
 
-  context[@"Window"][@"prototype"][@"webkit"][@"messageHandlers"][handlerName][@"postMessage"] =
-  ^(NSDictionary* message) {
-#ifdef DEBUG
-    //NSLog(@"%@ %@", handlerName, message);
-#endif
-    LegacyScriptMessage* msg = [LegacyScriptMessage new];
-    msg.writeableBody = message;
-    msg.writableName = handlerName;
-    msg.request = webview.request;
-    [handler userContentController:[WKUserContentController new] didReceiveScriptMessage:msg];
-  };
-
-
+    context[@"Window"][@"prototype"][@"webkit"][@"messageHandlers"][handlerName][@"postMessage"] =
+    ^(NSDictionary* message) {
+  #ifdef DEBUG
+      //NSLog(@"%@ %@", handlerName, message);
+  #endif
+      dispatch_async(dispatch_get_main_queue(), ^{
+        LegacyScriptMessage* msg = [LegacyScriptMessage new];
+        msg.writeableBody = message;
+        msg.writableName = handlerName;
+        msg.request = webview.request;
+        [handler userContentController:[WKUserContentController new] didReceiveScriptMessage:msg];
+      });
+    };
+  }
 }
 
 @end
