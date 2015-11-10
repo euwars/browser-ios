dd49930a79b6226a1e157b8c1b997dc54b1de71b
Garvan Keeley <garvankeeley@gmail.com>
2015-11-10 15:25:04 -0500

    Still getting complaints from xcode about the build-id.xcconfig missing. Some minor changes seem to quieten this.

--- brave/setup.sh
+++ brave/setup.sh
@@ -1,3 +1,6 @@
 [[ -e setup.sh  ]] || { echo 'setup.sh must be run from brave directory'; exit 1; }
 npm install
 (cd node_modules/abp-filter-parser-cpp && build/Release/sample)
+
+#output a placeholder id, Archive builds will generate a real build id
+echo GENERATED_BUILD_ID=1  > ../Client/Configuration/build-id.xcconfig
