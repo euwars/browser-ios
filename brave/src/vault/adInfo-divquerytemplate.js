963a8bba53b9716559693ee670f9a6379dae8691
Garvan Keeley <garvankeeley@gmail.com>
2015-11-17 10:49:41 -0500

    Issue #8, need to use ad-info w/h, not div w/h.

--- brave/src/vault/adInfo-divquerytemplate.js
+++ brave/src/vault/adInfo-divquerytemplate.js
@@ -1,14 +1,14 @@
 var domainInfo = _brave_adInfo['HOST'];
 
 domainInfo && JSON.stringify(domainInfo.map(function(x) {
-  var node = document.querySelector('[id=' + x.replaceId + ']:not([data-ad-replaced])');
-  node.setAttribute('data-ad-replaced', 1);
+  var node = document.querySelector('[id=' + x.replaceId + ']:not([data-ad-node-visited])');
+  node.setAttribute('data-ad-node-visited', 1);
   if (!node) { 
     return {};
   }
   return {
     'divId': x.replaceId,
-    'width': node.offsetWidth,
-    'height': node.offsetHeight
+    'width': x.width,
+    'height': x.height
   };
 }))
