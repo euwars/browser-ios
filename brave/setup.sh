ae247e3a4ec0aa66644d1214942a8bc4a0d1f33b
Garvan Keeley <garvankeeley@gmail.com>
2015-11-19 15:09:52 -0500

    Generate xcode project

--- brave/setup.sh
+++ brave/setup.sh
@@ -4,3 +4,9 @@ npm install
 
 #output a placeholder id, Archive builds will generate a real build id
 echo GENERATED_BUILD_ID=1  > ../Client/Configuration/build-id.xcconfig
+
+#create the xcode project
+python brave-proj.py
+
+echo "If files are added/removed from the project, regenerate it with ./brave-proj.py"
+
