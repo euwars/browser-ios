11d3a5bedf8a0fd5d12010da3e18cf7d602d3d41
Garvan Keeley <garvankeeley@gmail.com>
2015-11-23 21:09:59 -0500

    Add a post-checkout git script, which users can install. It runs on git pull for me, which is fabulous.

--- brave/setup.sh
+++ brave/setup.sh
@@ -1,5 +1,5 @@
 # Replace the removed xcconfigs with ours
-(cd ../Client && ln -sf ../brave/xcconfig Configuration)
+(cd ../Client && ln -sfn ../brave/xcconfig Configuration)
 
 [[ -e setup.sh  ]] || { echo 'setup.sh must be run from brave directory'; exit 1; }
 npm update
@@ -10,7 +10,8 @@ echo GENERATED_BUILD_ID=1  > xcconfig/build-id.xcconfig
 ## cp xcconfig/build-id.xcconfig ../Client/Configuration
 
 #create the xcode project
-python brave-proj.py
+python brave-proj.py > /dev/null 2>&1
 
+echo ""
 echo "If files are added/removed from the project, regenerate it with ./brave-proj.py"
-
+echo "Consider adding the post-checkout script for git automation (instructions are in that file)"
