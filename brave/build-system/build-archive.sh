6d41095055a660841102f7b306956a31f76e0f05
Garvan Keeley <garvankeeley@gmail.com>
2015-12-01 01:06:41 -0500

    update build script for latest code signing identity

--- brave/build-system/build-archive.sh
+++ brave/build-system/build-archive.sh
@@ -1,2 +1,2 @@
 (cd -- "$(dirname -- "$0")" && cd ../.. && \
- xcodebuild archive -scheme Brave CODE_SIGN_IDENTITY="iPhone Distribution: Brave Software, Inc. (KL7N8XSYF4)")
+ xcodebuild archive -scheme Brave CODE_SIGN_IDENTITY="iPhone Distribution: Brave Software, Inc. (KL8N8XSYF4)")
