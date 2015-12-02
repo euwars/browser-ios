61c5e53fe37a6e8964d9182abf31e9eb6df4ad8f
Garvan Keeley <garvankeeley@gmail.com>
2015-12-02 18:09:17 -0500

    Updating name of build dir for build script

--- brave/build-system/build-archive.sh
+++ brave/build-system/build-archive.sh
@@ -1,4 +1,4 @@
 [[ `which ios` ]] || gem install cupertino
-(cd provisioning-profiles && sh install-adhoc-profiles-from-portal.sh)
+(cd profiles && sh install-adhoc-profiles-from-portal.sh)
 (cd -- "$(dirname -- "$0")" && cd ../.. && \
  xcodebuild archive -scheme Brave CODE_SIGN_IDENTITY="iPhone Distribution: Brave Software, Inc. (KL8N8XSYF4)")
