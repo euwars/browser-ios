9654fa1591406688344c5c423cf63350a53e1c64
Garvan Keeley <garvankeeley@gmail.com>
2015-12-14 18:52:59 -0500

    Handy script to install developer profile from Apple

---
+++ brave/build-system/profiles/install-dev-profiles-from-portal.sh
@@ -0,0 +1,8 @@
+rm -rf ~/Library/MobileDevice/Provisioning\ Profiles/*
+rm /tmp/*.mobileprovision*
+
+USER=
+PASS=
+[[ -n $1 ]] && [[ -n $2 ]] && USER=" -u $1 " && PASS=" -p $2 " && echo "User and Pass specified"
+
+(cd /tmp && /usr/local/bin/ios profiles:download:all --team 9Y996D6DTQ --type development $USER $PASS)
