ad001ff928758bb34c49be163f4b7b0259eff37e
Garvan Keeley <garvankeeley@gmail.com>
2015-12-16 11:14:54 -0500

    Grab list of adhoc devices from portal to use for adhoc build validation

--- brave/build-system/profiles/adhoc/checkdevices.sh
+++ brave/build-system/profiles/adhoc/checkdevices.sh
@@ -1,9 +1,14 @@
+[[ -e ~/.brave-apple-login ]] && source ~/.brave-apple-login
+
+ios devices:list --team 9Y996D6DTQ $USER $PASS > /tmp/devices.txt
+
+lines=`awk 'NF>3{print $(NF-3)}' /tmp/devices.txt | egrep -v 'Listing|Identifier'`
 
 for f in `ls *.mobileprovision` 
 do
  echo "Processing $f"
 
- for line in `awk 'NF>1{print $NF}' devices.txt`
+ for line in $lines
  do
    `grep -q $line $f` || { echo "error $line not found"; exit 1; }    
  done
--- brave/build-system/profiles/install-adhoc-profiles-from-portal.sh
+++ brave/build-system/profiles/install-adhoc-profiles-from-portal.sh
@@ -5,6 +5,8 @@ USER=
 PASS=
 [[ -n $1 ]] && [[ -n $2 ]] && USER=" -u $1 " && PASS=" -p $2 " && echo "User and Pass specified"
 
+[[ -e ~/.brave-apple-login ]] && source ~/.brave-apple-login
+
 (cd adhoc && /usr/local/bin/ios profiles:download:all --team 9Y996D6DTQ --type distribution $USER $PASS)
 
 for f in `ls adhoc/*.mobileprovision`
