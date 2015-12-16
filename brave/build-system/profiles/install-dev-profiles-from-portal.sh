401cb0928a9fd99a19b5579777d739e35f147483
Garvan Keeley <garvankeeley@gmail.com>
2015-12-16 14:57:42 -0500

    add final install with xcode step to dev profile script

--- brave/build-system/profiles/install-dev-profiles-from-portal.sh
+++ brave/build-system/profiles/install-dev-profiles-from-portal.sh
@@ -6,3 +6,5 @@ PASS=
 [[ -n $1 ]] && [[ -n $2 ]] && USER=" -u $1 " && PASS=" -p $2 " && echo "User and Pass specified"
 
 (cd /tmp && /usr/local/bin/ios profiles:download:all --team 9Y996D6DTQ --type development $USER $PASS)
+# Since we are on a dev machine just use xcode to install these
+open /tmp/*.mobileprovision
