fd483d932ab63a68f123874b3a014e1604fb844e
Bradley Richter <bradr@Bradleys-MacBook-Pro-2.local>
2015-12-21 12:37:25 -0800

    new code and images to support initial top sites
    added assets for google, yahoo, bing, wikipedia, DDG

--- Storage/DefaultSuggestedSites.swift
+++ Storage/DefaultSuggestedSites.swift
@@ -8,18 +8,50 @@ public class DefaultSuggestedSites {
     public static let sites = [
       "default" : [
         SuggestedSiteData(
-            url: "https://www.brave.com",
-            bgColor: "0xf37c00",
-            imageUrl: "asset://suggestedsites_brave",
+            url: "https://www.google.com",
+            bgColor: "e6e6e6",
+            imageUrl: "asset://suggestedsites_google",
             faviconUrl: "asset://braveLogo",
             trackingId: 632,
-            title: NSLocalizedString("Brave Software", comment: "Tile title for Brave")
+            title: NSLocalizedString("Search Google", comment: "Tile title for Brave")
+        ),
+        SuggestedSiteData(
+            url: "https://www.yahoo.com",
+            bgColor: "420894",
+            imageUrl: "asset://suggestedsites_yahoo",
+            faviconUrl: "asset://braveLogo",
+            trackingId: 632,
+            title: NSLocalizedString("Search Yahoo", comment: "Tile title for Brave")
+        ),
+        SuggestedSiteData(
+            url: "https://www.bing.com",
+            bgColor: "808080",
+            imageUrl: "asset://suggestedsites_bing",
+            faviconUrl: "asset://braveLogo",
+            trackingId: 632,
+            title: NSLocalizedString("Search Bing", comment: "Tile title for Brave")
+        ),
+        SuggestedSiteData(
+            url: "https://www.duckduckgo.com",
+            bgColor: "ffffff",
+            imageUrl: "asset://suggestedsites_DDG",
+            faviconUrl: "asset://braveLogo",
+            trackingId: 632,
+            title: NSLocalizedString("Seach DuckDuckGo", comment: "Tile title for Brave")
+        ),
+        SuggestedSiteData(
+            url: "https://www.wikipedia.com",
+            bgColor: "e6e6e6",
+            imageUrl: "asset://suggestedsites_wiki",
+            faviconUrl: "asset://braveLogo",
+            trackingId: 632,
+            title: NSLocalizedString("Search Wikipedia", comment: "Tile title for Brave")
         ),
         SuggestedSiteData(
             url: "https://www.brave.com",
             bgColor: "0xf37c00",
-            imageUrl: "asset://suggestedsites_fxsupport",
-            faviconUrl: "asset://mozLogo",
+            imageUrl: "asset://suggestedsites_bravesupport",
+            faviconUrl: "asset://braveLogo",
             trackingId: 631,
             title: NSLocalizedString("Brave Help and Support", comment: "Tile title for App Help")
         )
