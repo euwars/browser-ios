3dfc283e1ca8c7686deaf88428f93b73bf80ac7f
Garvan Keeley <garvankeeley@gmail.com>
2016-01-06 15:23:34 -0500

    add MPL to brave sources

--- brave/src/vault/VaultAddressSetting.swift
+++ brave/src/vault/VaultAddressSetting.swift
@@ -1,3 +1,5 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
 import Shared
 
 // TODO this is just for development, figure out what to do with this setting
--- brave/src/vault/adInfo-wrapper.js
+++ brave/src/vault/adInfo-wrapper.js
@@ -1,3 +1,5 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
 /* eslint no-unused-vars: [2, {"varsIgnorePattern": "replaceDivWithNewContent"}] */
 
 function _brave_replaceDivWithNewContent(replacerObject) {
